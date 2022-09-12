2f4c63d05ddc9e6b75e6898b82d588d6a9379375
Kamil Przebieracz <dmjs23@gmail.com>
2022-09-12 12:52:19 +0200

    commit

--- contacts.js
+++ contacts.js
@@ -1,11 +1,59 @@
-const addContact = async (name, email, phone) => {
-  const contacts = await listContacts();
-  const newContact = {
-    name,
-    email,
-    phone,
+// filesystem
+const fs = require("fs");
+
+const contactsPath = "./db/contacts.json";
+
+// TODO: udokumentuj każdą funkcję
+function listContacts() {
+  fs.readFile(contactsPath, "utf8", (err, data) => {
+    if (err) {
+      console.log(`Error reading file from disk: ${err}`);
+    } else {
+      // parse JSON string to JSON object
+      const databases = JSON.parse(data);
+
+      // print all databases
+      databases.forEach((db) => {
+        console.log(`${db.id}: ${db.name}: ${db.email}: ${db.phone}`);
+      });
+    }
+  });
+}
+
+function getContactById(contactId) {
+  // ...twój kod
+}
+
+function removeContact(contactId) {
+  // ...twój kod
+}
+
+function addContact(name, email, phone) {
+  // ...twój kod
+  let id = 11;
+  let user = {
+    id: id,
+    name: name,
+    emai: email,
+    phone: phone,
   };
-  contacts.push(newContact);
-  await updateContacts(contacts);
-  return newContact;
+
+  // convert JSON object to a string
+  const data = JSON.stringify(user);
+
+  // write file to disk
+  fs.appendFile(contactsPath, data, "utf8", (err) => {
+    if (err) {
+      console.log(`Error writing file: ${err}`);
+    } else {
+      console.log(`File is written successfully!`);
+    }
+  });
+}
+
+module.exports = {
+  listContacts: listContacts,
+  getContactById: getContactById,
+  removeContact: removeContact,
+  addContact: addContact,
 };
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Hello world");
+
+const { listContacts, addContact } = require("./contacts");
+
+listContacts();
+// addContact("testowy", "testowy@test.pl", "123456789");
